d3188787cccc38e1947aae44caada9f19ed15786
Carlos <carlosalcaidevega@gmail.com>
2020-11-23 19:24:42 +0100

    primer comentario

---
+++ registrar.php
@@ -0,0 +1,11 @@
+
+<html>
+<body>
+   <p>   <?php echo 'El nombre es ' . $_POST [ 'nombre' ];  ?></p>
+   <p>   <?php echo 'El apellido es ' . $_POST [ 'apellidos' ];  ?></p>
+   <p>   <?php echo 'El edad es ' . $_POST [ 'edad' ];  ?></p>
+   <p>   <?php echo 'La dirección es ' . $_POST [ 'direccion' ];  ?></p>
+    <!-- AÑADE UN PÁRRAFO MÁS POR CADA CAMPO INPUT DEL FORMULARIO ANTERIOR-->
+    
+</body>
+</html>
